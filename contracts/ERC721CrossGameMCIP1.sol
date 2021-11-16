ed796af38fcaa5ba2b118e968772b5646c176162
Francesco Sullo <sullof@gmail.com>
2021-11-15 20:09:23 -0800

    Minor change

--- contracts/ERC721CrossGameMCIP1.sol
+++ contracts/ERC721CrossGameMCIP1.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Author: Francesco Sullo <francesco@sullo.co>
+// EverDragons2, https://everdragons2.com
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
