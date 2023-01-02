f06cac34f2f4ccd68d4e6136a77eb2358ec863d5
Francesco Sullo <sullof@gmail.com>
2023-01-01 16:36:31 -0800

    Fix deploy script

--- contracts/Everdragons2PFP.sol
+++ contracts/Everdragons2PFP.sol
@@ -1,16 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "@ndujalabs/erc721subordinate/contracts/ERC721EnumerableSubordinateUpgradeable.sol";
-//import "../subordinate/contracts/ERC721EnumerableSubordinateUpgradeable.sol";
+// Author: Francesco Sullo, francesco@sullo.co
+// January 1st, 2023
 
-contract Everdragons2PFP is ERC721EnumerableSubordinateUpgradeable {
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@ndujalabs/erc721subordinate/contracts/ERC721EnumerableSubordinateUpgradeable.sol";
+import "./Version.sol";
+
+contract Everdragons2PFP is Version, ERC721EnumerableSubordinateUpgradeable, OwnableUpgradeable {
+
+  event FrozeContract();
+  error ContractHasBeenFrozen();
+
+  string private _baseTokenURI;
+  bool public frozen;
+
   /// @custom:oz-upgrades-unsafe-allow constructor
   constructor() initializer {}
 
   function initialize(address everdragons2Genesis) public initializer {
-    __ERC721EnumerableSubordinateUpgradeable_init("Everdragons2 PFP", "E2PFP", everdragons2Genesis);
+    __Ownable_init();
+    __ERC721EnumerableSubordinate_init("Everdragons2 PFP", "E2PFP", everdragons2Genesis);
+    _baseTokenURI = "https://img.everdragons2.com/e2pfp/";
   }
 
-  function _authorizeUpgrade(address newImplementation) internal virtual override {}
+  function _authorizeUpgrade(address newImplementation) internal virtual override {
+    if (frozen) revert ContractHasBeenFrozen();
+  }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return _baseTokenURI;
+  }
+
+  function updateTokenURI(string memory uri) external onlyOwner {
+    if (frozen) revert ContractHasBeenFrozen();
+    _baseTokenURI = uri;
+  }
+
+  // Since ERC721Subordinate is a recent, un-audited proposal, we keep it initially
+  // upgradeable. freeze will allow us to freeze the contract, and make it no more
+  // upgradeable, when everything is written in stone.
+  function freeze() external onlyOwner {
+    // BE CAREFUL.
+    // This makes the contract not upgradeable anymore, and cannot be reverted.
+    frozen = true;
+  }
 }
---
+++ contracts/Version.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract Version {
+  function version() public pure virtual returns (string memory) {
+    // this must be aligned with the version in package.json
+    return "0.4.2";
+  }
+}
+
+
+
+
