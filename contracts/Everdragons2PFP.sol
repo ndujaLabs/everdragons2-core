44f980d0c1015a5cdeb86b3b2f97736e287a4315
Francesco Sullo <sullof@gmail.com>
2023-04-05 20:01:15 -0700

    Using new version of ERC721Subordinate

--- contracts/Everdragons2PFP.sol
+++ contracts/Everdragons2PFP.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "@ndujalabs/erc721subordinate/contracts/ERC721EnumerableSubordinateUpgradeable.sol";
-//import "../subordinate/contracts/ERC721EnumerableSubordinateUpgradeable.sol";
+import "@ndujalabs/erc721subordinate/contracts/ERC721SubordinateUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract Everdragons2PFP is ERC721EnumerableSubordinateUpgradeable {
+contract Everdragons2PFP is OwnableUpgradeable, ERC721SubordinateUpgradeable, UUPSUpgradeable {
   /// @custom:oz-upgrades-unsafe-allow constructor
   constructor() initializer {}
 
   function initialize(address everdragons2Genesis) public initializer {
-    __ERC721EnumerableSubordinateUpgradeable_init("Everdragons2 PFP", "E2PFP", everdragons2Genesis);
+    __ERC721Subordinate_init("Everdragons2 PFP", "E2PFP", everdragons2Genesis);
+    __UUPSUpgradeable_init();
+    __Ownable_init();
   }
 
   function _authorizeUpgrade(address newImplementation) internal virtual override {}
